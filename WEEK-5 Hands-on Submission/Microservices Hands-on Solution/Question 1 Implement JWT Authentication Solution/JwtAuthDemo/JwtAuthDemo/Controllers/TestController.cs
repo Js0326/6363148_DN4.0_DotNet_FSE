20d7daf93d4df6f5e7220b85248baecd224a1e40
Jatin Sharma <jatinsharma0326@gmail.com>
2025-07-20 23:48:34 +0530

    Add Week-5 Solution Files

---
+++ WEEK-5 Hands-on Submission/Microservices Hands-on Solution/Question 1 Implement JWT Authentication Solution/JwtAuthDemo/JwtAuthDemo/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("secure")]
+        [Authorize]
+        public IActionResult Secure()
+        {
+            return Ok($"You are authenticated as: {User.Identity?.Name}");
+        }
+    }
+}
